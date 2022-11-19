a56ef3021c35ee2279ebfe287b3698c830c5e8c9
N-Scheider <noah.zh.01@gmail.com>
2022-11-19 21:01:17 +0100

    Sampling for K folds

---
+++ Implementation_PCA_Noah.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(caret)
+
+# Dataframe as input
+df <- c()
+
+
+# Create folds for Cross-Validation
+
+df_df <- data.frame("obs1"=c(5,43,2,3), "obs2"=c(5,6,7,3))
+df <- as.matrix(df_df)
+n <- dim(df)[1]
+index <- c(1:n)
+K <- 2
+samples <- vector("list", K)
+
+for (i in 1:K) {
+  if (i < K) {
+    fold <- sample(index, n/K)
+    samples[[i]] <- fold
+    index <- index[-fold]
+  }
+  else{
+    samples[[i]] <- index
+  }
+}
+
+# Index can now be used to slice df in every step of cross-validation step
+
+
