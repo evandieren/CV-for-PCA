c2e10cfecaac8f0b4e1e45194b343ae40bb4f114
N-Scheider <noah.zh.01@gmail.com>
2022-11-25 17:26:11 +0100

    Method 3 and 4

--- Implementation_PCA_Noah.R
+++ Implementation_PCA_Noah.R
@@ -1,6 +1,7 @@
 # mvtnorm provides possibility to create multivariate gaussian rv
 library(mvtnorm)
 
+
 # Dataframe as input
 df <- c()
 
@@ -19,7 +20,6 @@ for (i in 1:K) {
   if (i < K) {
     fold <- sample(index, n/K)
     samples[[i]] <- fold
-    print(which(fold %in% index))
     index <- index[-which(index %in% fold)]
   }
   else{
@@ -32,7 +32,7 @@ for (i in 1:K) {
 # How is r set? r < p
 r <- 2
 
-# Sample randomly r columns
+# Sample randomly p-r columns in order to keep r principal columns
 trunc <- sample(1:p, p-r)
 
 
@@ -42,7 +42,7 @@ trunc <- sample(1:p, p-r)
 # Split data into missing and observed data on jth component
 # e.g. split missing and observed data in half, or split variables randomly
 # split <- sample(1:p, ceiling(ratio)), ratio < r as data is truncated to rank r
-split <- 1:ceiling(r/2)
+split <- sample(1:p, ceiling(p/2))
 
 mse1 <- rep(0, K)
 
@@ -51,18 +51,17 @@ for (k in 1:K) {
   
   df_k <- df[-samples[[k]],]
   mu <- colMeans(df_k)
-  mu_trunc <- mu[-trunc]
-  sigma <- cov(df_k)
-  sigma_trunc <- sigma[-trunc,-trunc]
+  svd_sigma <- svd(cov(df_k))
+  sigma_trunc <- svd_sigma$u[,-trunc] %*% diag(svd_sigma$d[-trunc]) %*% t(svd_sigma$v)[-trunc,]
   
   df_k_fold <- df[samples[[k]],]
-  df_k_fold_trunc <- df_k_fold[,-trunc]
-  df_k_fold_miss <- as.matrix(df_k_fold_trunc[,split])
-  df_k_fold_obs <- as.matrix(df_k_fold_trunc[,-split])
+  df_k_fold_miss <- as.matrix(df_k_fold[,split])
+  df_k_fold_obs <- as.matrix(df_k_fold[,-split])
   
   # Estimate est_x_miss for df_k_fold_miss
-  mu_miss <- mu_trunc[split]
-  mu_obs <- mu_trunc[-split]
+  mu_miss <- mu[split]
+  mu_obs <- mu[-split]
+  
   sigma_miss_obs <- sigma_trunc[split, -split]
   sigma_obs_obs <- sigma_trunc[-split, -split]
 
@@ -93,8 +92,7 @@ for (k in 1:K) {
   mu <- colMeans(df_k)
   svd_sigma <- svd(cov(df_k))
   sigma_trunc <- svd_sigma$u[,-trunc] %*% diag(svd_sigma$d[-trunc]) %*% t(svd_sigma$u[,-trunc])
-  cov(df_k)
-  
+
   df_k_fold <- df[samples[[k]],]
   df_k_fold_miss <- as.matrix(df_k_fold[,split])
   df_k_fold_obs <- as.matrix(df_k_fold[,-split])
@@ -114,14 +112,70 @@ for (k in 1:K) {
   
 }
 
-mse1
-mse2
+
+# KDE Approach
+
+trunc <- sample(1:p, p-r)
+
+KDEApproach <- function(X, pca_vec){
+  # args: X matrix containing data, pca_vec containing index of pca (dimension r)
+  # returns: error measure of truncated covariance to the real covariance (Frobenius norm)
+  
+  sigma <- cov(X)
+  svd_sigma <- svd(sigma)
+  sigma_trunc <- svd_sigma$u[,pca_vec] %*% diag(svd_sigma$d[pca_vec]) %*% t(svd_sigma$v)[pca_vec,]
+  
+  return(norm(sigma-sigma_trunc, type = "F"))
+}
+
+KDEApproach(df, 1:2)
 
 
-#d <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow = 3, ncol = 3)
-#d1 <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow = 3, ncol = 3)
-#da <- c(1, 2, 3)
-#d1 %*% da
-#d %*% d1
-#colMeans(d)
-#cov(d)
+MatrixCompletion <- function(X, pca_vec){
+  # args: X matrix containing data, pca_vec containing index of pca (dimension r)
+  # returns: error measure of truncated covariance to the real covariance (Frobenius norm)
+  
+  # Create bivariate index set for observed data
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  biv_index <- matrix(sample(0:1, size = n*p, replace = T), nrow = n, ncol = p)
+  
+  # Iterative-hard thresholding algorithm
+  M <- X
+  for (i in 1:10) {
+    svd_M <- svd(M)
+    M_trunc <- svd_M$u[,pca_vec] %*% diag(svd_M$d[pca_vec]) %*% t(svd_M$v)[pca_vec,]
+    M <- X * biv_index + M_trunc * !biv_index
+  }
+  
+  return(norm(X-M, type = "F"))
+}
+  
+MatrixCompletion(df, 1:2)
+
+
+
+
+# Question:
+
+# 25.11.22
+# How is r choosen? -> PCA first
+# How missing and observed data split? -> randomly, but rule of thumb is half half
+# MSE on x_miss? -> Yes
+# Less informations in Method 1? -> No, different computations of mu and sigma
+# Method 1: Truncating Missed and Observed data accordingly? -> No
+# Method 2: Still p dimensions? -> Yes
+# Method 3: How to cross-validate? Different error measure of covariance similiarity
+
+# ..
+# Method: Splitting missing and observed data differently in Folds?
+
+
+
+
+
+
+
+
+
+
