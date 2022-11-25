a24ab533fc8b6ae902f430e9f685b6875e4ebb18
N-Scheider <noah.zh.01@gmail.com>
2022-11-25 21:48:55 +0100

    "Wrong Method", "Method 1 corrected", "Method 3", "Method 4", prcomp(), multivariate_data

--- Implementation_PCA_Noah.R
+++ Implementation_PCA_Noah.R
@@ -2,77 +2,126 @@
 library(mvtnorm)
 
 
-# Dataframe as input
-df <- c()
-
-
-# Create folds for Cross-Validation
-
-df_df <- data.frame("obs1"=c(5,43,2,3,1,4,8,4,5,9), "obs2"=c(5,6,7,3,9,9,12,1,5,8), "obs3"=c(1,2,5,3,4,5,6,1,5,7), "obs4"=c(1,7,6,3,4,5,6,1,7,10))
+df_df <- data.frame("obs1"=c(5,43,2,3,1,4,8,4,5,9), "obs2"=c(5,43,2,3,1,4,8,4,5,9), "obs3"=c(1,2,5,3,4,5,6,1,5,7), "obs4"=c(1,7,6,3,4,5,6,1,7,10))
 df <- as.matrix(df_df)
-n <- dim(df)[1]
-p <- dim(df)[2]
-index <- c(1:n)
-K <- 3
-samples <- vector("list", K)
 
-for (i in 1:K) {
-  if (i < K) {
-    fold <- sample(index, n/K)
-    samples[[i]] <- fold
-    index <- index[-which(index %in% fold)]
+
+# Create Multivariate Random Variables
+sigma <- matrix(c(5,43,2,3,1,4,8,4,5,9,5,43,2,3,1,4,8,4,5,9,5,43,2,3,1), nrow = 5, ncol = 5)
+df1 <- rmvnorm(n = 20, mean = rep(0, nrow(sigma)), sigma = sigma %*% t(sigma))
+
+
+# R built-in function for PCA
+pca3 <- prcomp(df1, rank. = 5)
+summary(pca3) # overview of the extent a variable explains the data
+pca3$sdev # array sorted by Principal components that explain % of standard deviation
+
+
+WrongPCA <- function(X, pca_vec, K){
+  # args: X matrix containing data, pca_vec containing index of pca (dimension r), K amount of CV-Folds
+  # returns: MSE of the CV
+  
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  index <- c(1:n)
+  K <- K
+  samples <- vector("list", K)
+  
+  for (i in 1:K) {
+    if (i < K) {
+      fold <- sample(index, n/K)
+      samples[[i]] <- fold
+      index <- index[-which(index %in% fold)]
+    }
+    else{
+      samples[[i]] <- index
+    }
   }
-  else{
-    samples[[i]] <- index
+  
+  mse <- rep(0, K)
+  
+  for (k in 1:K) {
+    l1 <- length(samples[[k]])
+    df_k <- X[samples[[k]], ]
+    
+    svd_X <- svd(X)
+    X_trunc <- svd_X$u[,pca_vec] %*% diag(svd_X$d[pca_vec]) %*% t(svd_X$v)[pca_vec,]
+
+    # Define projection and project data
+    P <- X_trunc %*% solve(t(X_trunc) %*% X_trunc, tol = 1e-20) %*% t(X_trunc)
+    X_proj <- P %*% X
+    df_k_proj <- X_proj[samples[[k]],]
+    
+    # Error of estimated and true missing observation
+    for(s in 1:l1){
+      mse[k] <- mse[k] + norm(df_k[s,] - df_k_proj[s,], type = "2")^2/l1/K
+    }
   }
+  
+  return(sum(mse))
+
 }
 
-# Index can now be used to slice df in every step of cross-validation
-
-# How is r set? r < p
-r <- 2
-
-# Sample randomly p-r columns in order to keep r principal columns
-trunc <- sample(1:p, p-r)
+WrongPCA(df1, 1:3, 5)
 
 
-
-# Cross-Validation: "Artificially turn the unsupervised problem into a supervised one"
-
-# Split data into missing and observed data on jth component
-# e.g. split missing and observed data in half, or split variables randomly
-# split <- sample(1:p, ceiling(ratio)), ratio < r as data is truncated to rank r
-split <- sample(1:p, ceiling(p/2))
-
-mse1 <- rep(0, K)
-
-for (k in 1:K) {
-  l1 <- length(samples[[k]])
+WrongPCAImproved <- function(X, pca_vec, K){
+  # args: X matrix containing data, pca_vec containing index of pca (dimension r), K amount of CV-Folds
+  # returns: MSE of the CV
   
-  df_k <- df[-samples[[k]],]
-  mu <- colMeans(df_k)
-  svd_sigma <- svd(cov(df_k))
-  sigma_trunc <- svd_sigma$u[,-trunc] %*% diag(svd_sigma$d[-trunc]) %*% t(svd_sigma$v)[-trunc,]
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  index <- c(1:n)
+  K <- K
+  samples <- vector("list", K)
   
-  df_k_fold <- df[samples[[k]],]
-  df_k_fold_miss <- as.matrix(df_k_fold[,split])
-  df_k_fold_obs <- as.matrix(df_k_fold[,-split])
-  
-  # Estimate est_x_miss for df_k_fold_miss
-  mu_miss <- mu[split]
-  mu_obs <- mu[-split]
-  
-  sigma_miss_obs <- sigma_trunc[split, -split]
-  sigma_obs_obs <- sigma_trunc[-split, -split]
-
-  est_x_miss <- lapply(1:l1, function(n){mu_miss+sigma_miss_obs%*%solve(sigma_obs_obs)%*%(df_k_fold_obs[n,]-mu_obs)})
-  
-  # Error of estimated and true missing observation
-  for(s in 1:l1){
-    mse1[k] <- mse1[k] + norm(est_x_miss[[s]]-df_k_fold_miss[s,], type = "2")^2/l1/K
+  for (i in 1:K) {
+    if (i < K) {
+      fold <- sample(index, n/K)
+      samples[[i]] <- fold
+      index <- index[-which(index %in% fold)]
+    }
+    else{
+      samples[[i]] <- index
+    }
   }
+  
+  # Split data into missing and observed data
+  split <- sample(1:p, floor(p/2))
+  
+  mse1 <- rep(0, K)
+  
+  for (k in 1:K) {
+    l1 <- length(samples[[k]])
+    
+    df_k <- X[-samples[[k]],]
+    mu <- colMeans(df_k)
+    svd_sigma <- svd(cov(df_k))
+    sigma_trunc <- svd_sigma$u[,pca_vec] %*% diag(svd_sigma$d[pca_vec]) %*% t(svd_sigma$v)[pca_vec,]
+    
+    df_k_fold <- X[samples[[k]],]
+    df_k_fold_miss <- as.matrix(df_k_fold[,split])
+    df_k_fold_obs <- as.matrix(df_k_fold[,-split])
+    
+    # Estimate est_x_miss for df_k_fold_miss
+    mu_miss <- mu[split]
+    mu_obs <- mu[-split]
+    
+    sigma_miss_obs <- sigma_trunc[split, -split]
+    sigma_obs_obs <- sigma_trunc[-split, -split]
+    
+    est_x_miss <- lapply(1:l1, function(n){mu_miss+sigma_miss_obs%*%solve(sigma_obs_obs)%*%(df_k_fold_obs[n,]-mu_obs)})
+    
+    # Error of estimated and true missing observation
+    for(s in 1:l1){
+      mse1[k] <- mse1[k] + norm(est_x_miss[[s]]-df_k_fold_miss[s,], type = "2")^2/l1/K
+    }
+  }
+  
+  return(sum(mse1))
 }
 
+WrongPCAImproved(df1, 1:2, 5)
 
 
 # Cross-Validation: "Missing Data Approach"
@@ -113,10 +162,6 @@ for (k in 1:K) {
 }
 
 
-# KDE Approach
-
-trunc <- sample(1:p, p-r)
-
 KDEApproach <- function(X, pca_vec){
   # args: X matrix containing data, pca_vec containing index of pca (dimension r)
   # returns: error measure of truncated covariance to the real covariance (Frobenius norm)
@@ -128,7 +173,7 @@ KDEApproach <- function(X, pca_vec){
   return(norm(sigma-sigma_trunc, type = "F"))
 }
 
-KDEApproach(df, 1:2)
+KDEApproach(df1, 1:2)
 
 
 MatrixCompletion <- function(X, pca_vec){
@@ -151,7 +196,7 @@ MatrixCompletion <- function(X, pca_vec){
   return(norm(X-M, type = "F"))
 }
   
-MatrixCompletion(df, 1:2)
+MatrixCompletion(df1, 1:2)
 
 
 
@@ -168,7 +213,9 @@ MatrixCompletion(df, 1:2)
 # Method 3: How to cross-validate? Different error measure of covariance similiarity
 
 # ..
-# Method: Splitting missing and observed data differently in Folds?
+# Method 0: Correctly implemented?
+# Method 1: Splitting missing and observed data differently in Folds?
+# Method 4: When is convergence achieved? How to choose tolerance?
 
 
 
