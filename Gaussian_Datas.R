a8fdd8089009fdaa8817b0eb4170c0d54ffd3e41
lelesorgi <emas1@hotmail.it>
2022-12-06 17:40:10 +0100

    Commit

--- Gaussian_Datas.R
+++ Gaussian_Datas.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(mvtnorm)
 # create data vectors
 sample_size <- 1000                                       
@@ -14,20 +13,3 @@ sample_distribution <- rmvnorm(n = sample_size,
 head(sample_distribution)
 #sample_distribution
 #It has no sense to plot because we have 5 dimensions
-
-=======
-library(MASS)
-# create data vectors
-sample_size <- 1000                                       
-sample_meanvector <- c(10, -2, 5, 10, 20)                                   
-sample_covariance_matrix <- matrix(c(7, 4, 3, 2, 1, 4, 5, 4, 3, 2,
-                                     3, 4, 5, 4, 3, 2, 3, 4, 5, 4, 1, 
-                                     2, 3, 4, 9), ncol = 5)
-# create multivariate normal distribution
-sample_distribution <- mvrnorm(n = sample_size,
-                               mu = sample_meanvector, 
-                               Sigma = sample_covariance_matrix)
-# print distribution
-head(sample_distribution)
-#sample_distribution
->>>>>>> 320d26ddce8e291c3956038ebe2c1213caf83d97
