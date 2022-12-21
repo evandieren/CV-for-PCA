2d8adc8076f02d68800dc76e03ddce07922242e1
Eliott Van Dieren <vandieren.eliott@gmail.com>
2022-12-21 14:26:00 +0100

    modifs almond matrixcompletion

--- MatrixCompletion.R
+++ MatrixCompletion.R
@@ -13,30 +13,28 @@ MatrixCompletion <- function(X){
   mse5 <- rep(0, p)
   biv_index <- matrix(sample(0:1, size = n*p, replace = T), nrow = n, ncol = p)
   
-  # Iterative-hard thresholding algorithm
-  tol = 1e-4
-  for (r in 1:p) {
-    steps <- 0
-    M_old <- matrix(rep(1,n*p), ncol=p)
-    M_new <- X
-    while(norm(M_new-M_old, type = "F")/norm(M_new, type="F") > tol){
-      M_old = M_new
-      svd_M <- svd(M_old)
-      svd_M$d[-(1:r)] <- 0
-      M_trunc <- svd_M$u %*% diag(svd_M$d) %*% t(svd_M$v)
-      M_new <- X * biv_index + M_trunc * !biv_index
-      steps <- steps + 1
+  miss_X <- X
+  for (i in 1:n){
+    for (j in 1:p){
+      if (biv_index[i,j] == 0){
+        miss_X[i,j] <- NA
+      }
     }
-    print(paste0("MatrixCompletion: Steps ", steps, ", for rank r ", r))
+  }
+  
+  print(miss_X)
+  
+  # Iterative-hard thresholding algorithm
+  for (r in 1:p) {
+    M_new <- MatrixCompletion_only(miss_X,X,r)
     mse5[r] <- norm(X-M_new, type = "F")
   }
   return(mse5)
 }
 
-
 # ALMOND: extract only core function for clearity --------------------------
 
-MatrixCompletion_only <- function(X, X0, r, tol = 1e-4, maxsteps = 100){
+MatrixCompletion_only <- function(X, X0, r, tol = 1e-4, maxsteps = 1000){
   # args: X: matrix containing data - missing values are NA
   # X0: complete matrix of the same format as X, initial value for algorithm
   # tol, maxsteps: error tolerance and maximum steps, so algo doesn't run forever
